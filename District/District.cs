820505451cfb85514fc78377acac4b3e73a226b1
thedigitalmenagerie <honeyraeswan@gmail.com>
2021-08-10 20:16:56 -0400

    adding base classes

---
+++ District/District.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hagrid_QuikTrip.District
+{
+    class District
+    {
+        public string Name { get; set; }
+        public int DistrictID { get; set; }
+        public double QuarterlyDistrictSales { get; set; }
+        public double YearlyDistrictSales { get; set; }
+        public District(string name, int districtID)
+        {
+            Name = name;
+            DistrictID = districtID;
+        }
+    }
+}
