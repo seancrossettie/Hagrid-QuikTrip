57d600479d5144373c59f72bf2ace31e21377f3e
thedigitalmenagerie <honeyraeswan@gmail.com>
2021-08-10 21:20:45 -0400

    inheriting classes

---
+++ Store/Store.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hagrid_QuikTrip.Store
+{
+    class Store
+    {
+        public int StoreID { get; set; }
+        public int DistrictID { get; set; }
+        public double GasCurrentQuarterlySales { get; set; } = 0;
+        public double GasCurrentYearlySales { get; set; } = 0;
+        public double QuarterlySales { get; set; } = 0;
+        public double YearlySales { get; set; } = 0;
+
+        public Store(int storeID)
+        {
+            StoreID = storeID;
+        }
+
+        public Store(int storeID, int districtID)
+        {
+            StoreID = storeID;
+            DistrictID = districtID;
+        }
+    }
+}
