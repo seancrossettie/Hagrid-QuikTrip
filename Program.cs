be54a864f526381102e2b73fdf5b7862fb94fa1d
seancrossettie <seancrossettie@gmail.com>
2021-08-10 20:38:19 -0500

    merged with main

--- Program.cs
+++ Program.cs
@@ -6,7 +6,40 @@ namespace Hagrid_QuikTrip
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to the QuikTrip Sales Management System");
+            Console.WriteLine();
+            Console.WriteLine("Please choose an option below:");
+            Console.WriteLine("1. Enter District Sales");
+            Console.WriteLine("2. Generate District Report");
+            Console.WriteLine("3. Add New Employee");
+            Console.WriteLine("4. Add a Store/District");
+            Console.WriteLine("5. Exit");
+            Console.WriteLine();
+            Console.Write("\r\nEnter an option: ");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    Console.Write("\r\nEnter sales from one of the following districts: ");
+                    Console.ReadLine();
+                    break;
+                case "2":
+                    Console.WriteLine("2");
+                    break;
+                case "3":
+                    Console.WriteLine("3");
+                    break;
+                case "4":
+                    Console.WriteLine("4");
+                    break;
+                case "5":
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine();
+                    break;
+            }
+
         }
     }
 }
